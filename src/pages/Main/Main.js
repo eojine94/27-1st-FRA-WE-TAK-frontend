37854b670999752c8e5872cfd1c5820d327a58d6
leejihyun <ix219@naver.com>
2021-11-29 21:40:58 +0900

    Add : add fontawesome link and rename branch

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,7 +3,11 @@ import Nav from '../../components/Nav/Nav';
 import './Main.scss';
 
 function Main() {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <Nav />
+    </div>
+  );
 }
 
 export default Main;

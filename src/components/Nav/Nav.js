c7a9cf0905e2fc4618f1567b090fb715d4f34b9f
leejihyun <ix219@naver.com>
2021-11-30 21:22:17 +0900

    Modify : CamelCase, className, margin padding

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,29 +4,29 @@ import './Nav.scss';
 function Nav() {
   return (
     <div className="nav">
-      <div className="nav_title">
-        <span className="green_color">Frei</span>tak
-        <span className="green_color">.</span>
+      <div className="navTitle">
+        <span className="greenColor">Frei</span>tak
+        <span className="greenColor">.</span>
       </div>
-      <ul className="nav_menu">
-        <li className="chicken_menu">CHICKEN</li>
-        <li className="egg_logo">
+      <ul className="navMenu">
+        <li className="menu">CHICKEN</li>
+        <li className="eggLogo">
           <img
-            className="egg_logo_img"
+            className="eggLogoImg"
             src="../image/egg_character2.jpg"
-            alt="egg_logo"
+            alt="eggLogo"
           />
         </li>
-        <li className="dinosaur_menu">DINOSAUR</li>
+        <li className="menu">DINOSAUR</li>
       </ul>
-      <ul className="nav_icon">
-        <li className="user_icon">
+      <ul className="navIcon">
+        <li className="icon">
           <i className="fas fa-user" />
         </li>
-        <li className="shopping_cart_icon">
+        <li className="icon">
           <i className="fas fa-shopping-cart" />
         </li>
-        <li className="search_icon">
+        <li className="icon">
           <i className="fas fa-search" />
         </li>
       </ul>

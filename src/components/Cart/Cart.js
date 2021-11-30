cdba4894de3eea027d61dbe00f647fce7f1164bb
leejihyun <ix219@naver.com>
2021-11-30 21:40:18 +0900

    Add : Shopping-Cart Layout

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,79 @@ import React from 'react';
 import './Cart.scss';
 
 function Cart() {
-  return <div className="cart" />;
+  return (
+    <div className="cart">
+      <h2 className="cartListTitle">Cart List</h2>
+      <ul className="cartDetail">
+        <li className="itemPhoto">photo</li>
+        <li className="itemNum">num</li>
+        <li className="itemPrice">price</li>
+        <li className="itemDelete">delete</li>
+      </ul>
+      <div class="itemList">
+        <ul className="itemDetail">
+          <li className="itemPhoto">
+            <img
+              className="eggItemImage"
+              src="../../image/logo.png"
+              alt="eggItem"
+            />
+          </li>
+          <li className="itemNum">1</li>
+          <li className="itemPrice">8,000</li>
+          <li className="itemDelete">x</li>
+        </ul>
+        <ul className="itemDetail">
+          <li className="itemPhoto">
+            <img
+              className="eggItemImage"
+              src="../../image/logo.png"
+              alt="eggItem"
+            />
+          </li>
+          <li className="itemNum">1</li>
+          <li className="itemPrice">5,000</li>
+          <li className="itemDelete">x</li>
+        </ul>
+        <ul className="itemDetail">
+          <li className="itemPhoto">
+            <img
+              className="eggItemImage"
+              src="../../image/logo.png"
+              alt="eggItem"
+            />
+          </li>
+          <li className="itemNum">2</li>
+          <li className="itemPrice">1000,000</li>
+          <li className="itemDelete">x</li>
+        </ul>
+        <ul className="itemDetail">
+          <li className="itemPhoto">
+            <img
+              className="eggItemImage"
+              src="../../image/logo.png"
+              alt="eggItem"
+            />
+          </li>
+          <li className="itemNum">1</li>
+          <li className="itemPrice">5,000</li>
+          <li className="itemDelete">x</li>
+        </ul>
+        <ul className="itemDetail">
+          <li className="itemPhoto">
+            <img
+              className="eggItemImage"
+              src="../../image/logo.png"
+              alt="eggItem"
+            />
+          </li>
+          <li className="itemNum">1</li>
+          <li className="itemPrice">5,000</li>
+          <li className="itemDelete">x</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,36 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Cart from '../Cart/Cart';
 import './Nav.scss';
 
 function Nav() {
+  const [cart, setCart] = useState(false);
+
+  function onClickCart() {
+    setCart(!cart);
+  }
+
   return (
-    <div className="nav">
-      <div className="navTitle">
-        <span className="greenColor">Frei</span>tak
-        <span className="greenColor">.</span>
+    <>
+      <div className="nav">
+        <div className="navTitle">
+          <span className="greenColor">Frei</span>tak
+          <span className="greenColor">.</span>
+        </div>
+        <ul className="navMenu">
+          <li className="menu">CHICKEN</li>
+          <li className="eggLogo">
+            <img
+              className="eggLogoImg"
+              src="../image/egg_character2.jpg"
+              alt="eggLogo"
+            />
+          </li>
+          <li className="menu">DINOSAUR</li>
+        </ul>
+        <ul className="navIcon">
+          <li className="icon">
+            <i className="fas fa-user" />
+          </li>
+          <li className="icon" onClick={onClickCart}>
+            <i className="fas fa-shopping-cart" />
+          </li>
+          <li className="icon">
+            <i className="fas fa-search" />
+          </li>
+        </ul>
       </div>
-      <ul className="navMenu">
-        <li className="menu">CHICKEN</li>
-        <li className="eggLogo">
-          <img
-            className="eggLogoImg"
-            src="../image/egg_character2.jpg"
-            alt="eggLogo"
-          />
-        </li>
-        <li className="menu">DINOSAUR</li>
-      </ul>
-      <ul className="navIcon">
-        <li className="icon">
-          <i className="fas fa-user" />
-        </li>
-        <li className="icon">
-          <i className="fas fa-shopping-cart" />
-        </li>
-        <li className="icon">
-          <i className="fas fa-search" />
-        </li>
-      </ul>
-    </div>
+      {cart === true ? <Cart /> : null}
+    </>
   );
 }
 

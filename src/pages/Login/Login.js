86434267963e2c1c2b940dfeb290e3c7be769c8b
niboo <moonevening8@gmail.com>
2021-12-02 14:35:59 +0900

    REFACTOR: 리뷰반영 수정

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Login.scss';
 
 function Login() {
+  // const [emailValue, setEmailValue] = useState('');
+  // const [passwordValue, setPasswordValue] = useState('');
+
+  const [inputValue, setInputValue] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = inputValue;
+
+  const [isActiveEmailBorder, setIsActiveEmailBorder] = useState(false);
+  const [isActivePasswordBorder, setIsActivePasswordBorder] = useState(false);
+
+  const handleInput = event => {
+    const { name, value } = event.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+
+  const isValidInput = () => {
+    return email.length >= 1 && password.length >= 1;
+  };
+  const handleButtonAction = () => {
+    return isValidInput() ? 'loginButtonAction' : 'loginButtonInaction';
+  };
+
   return (
-    <main className="Login">
+    <main className="login">
       <div className="loginTop">
         <h1 className="loginTitle">LOG IN YOUR USER ACCOUNT</h1>
       </div>
@@ -11,21 +40,42 @@ function Login() {
         <form className="loginInput">
           <div className="emailInput">
             <div className="loginInputMassage">Email address *</div>
-            <input className="email" />
+            <input
+              name="email"
+              className={isActiveEmailBorder ? 'clickedInputLine' : 'inputLine'}
+              onChange={handleInput}
+              onFocus={() => setIsActiveEmailBorder(true)}
+              onBlur={() => setIsActiveEmailBorder(false)}
+            />
           </div>
           <div className="passwordInput">
             <div className="loginInputMassage">Password *</div>
-            <input className="password" />
+            <input
+              name="password"
+              className={
+                isActivePasswordBorder ? 'clickedInputLine' : 'inputLine'
+              }
+              onChange={handleInput}
+              onFocus={() => setIsActivePasswordBorder(true)}
+              onBlur={() => setIsActivePasswordBorder(false)}
+            />
           </div>
           <span className="forgotPassword line">Forgot your password?</span>
         </form>
       </div>
       <div className="loginBottom">
-        <button className="loginButton" type="button">
+        <button
+          className={handleButtonAction()}
+          type="button"
+          disabled={!isValidInput()}
+        >
           LOG IN
         </button>
         <span className="join">
-          Not a member yet? <span className="line">Join us.</span>
+          Not a member yet?{' '}
+          <Link to="/signup">
+            <span className="line">Join us.</span>
+          </Link>
         </span>
       </div>
     </main>
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,9 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignUp.scss';
 
 function SignUp() {
+  const [inputValue, setInputValue] = useState({
+    userName: '',
+    email: '',
+    password: '',
+    phoneNumber: '',
+    address: '',
+  });
+
+  const { userName, email, password, phoneNumber, address } = inputValue;
+
+  const [activeBorder, setActiveBorder] = useState({
+    userNameBorder: false,
+    emailBorder: false,
+    passwordBorder: false,
+    phoneNumberBorder: false,
+    addressBorder: false,
+  });
+
+  const {
+    userNameBorder,
+    emailBorder,
+    passwordBorder,
+    phoneNumberBorder,
+    addressBorder,
+  } = activeBorder;
+
+  const [checkBoxActive, setCheckboxActive] = useState(false);
+
+  const handleInput = event => {
+    const { name, value } = event.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+
+  const handleFocusBorder = border => {
+    setActiveBorder({
+      ...activeBorder,
+      [border]: true,
+    });
+  };
+  const handleBlurBorder = border => {
+    setActiveBorder({
+      ...activeBorder,
+      [border]: false,
+    });
+  };
+
+  const isValidInput = () => {
+    return (
+      userName.length >= 1 && phoneNumber.length >= 1 && address.length >= 1
+    );
+  };
+
+  const isValidEmail = () => {
+    return email.includes('@') && email.includes('.');
+  };
+
+  const specialLetter = password.search(/[`~!@@#$%^&*|₩₩₩'₩";:₩/?]/gi);
+
+  const isValidPassword = () => {
+    return password.length >= 8 && specialLetter >= 1;
+  };
+  const isClicked = () => {
+    setCheckboxActive(!checkBoxActive);
+    console.log(checkBoxActive);
+  };
+  const getIsActive = () => {
+    return (
+      isValidEmail() &&
+      isValidPassword() &&
+      isValidInput() &&
+      checkBoxActive === true
+    );
+  };
+  const handleButtonAction = () => {
+    return getIsActive() ? 'signUpButtonAction' : 'signUpButtonInaction';
+  };
   return (
-    <main className="SignUp">
+    <main className="signUp">
       <div className="signUpTop">
         <h1 className="signUpTitle">NEW HERE? CREATE YOUR USER ACCOUNT</h1>
         <p className="signUpContent">
@@ -15,28 +94,59 @@ function SignUp() {
         <form className="signUpInput">
           <div className="nameInput">
             <div className="inputMessage">Name *</div>
-            <input className="name" />
+            <input
+              className={userNameBorder ? 'clickedInputLine' : 'inputLine'}
+              name="userName"
+              onChange={handleInput}
+              onFocus={() => handleFocusBorder('userNameBorder')}
+              onBlur={() => handleBlurBorder('userNameBorder')}
+            />
           </div>
           <div className="emailInput">
             <div className="inputMessage">Email(ID) *</div>
-            <input className="email" />
+            <input
+              name="email"
+              className={emailBorder ? 'clickedInputLine' : 'inputLine'}
+              onChange={handleInput}
+              onFocus={() => handleFocusBorder('emailBorder')}
+              onBlur={() => handleBlurBorder('emailBorder')}
+            />
           </div>
           <div className="passwordInput">
             <div className="inputMessage">Password *</div>
-            <input className="password" />
+            <input
+              type="password"
+              name="password"
+              className={passwordBorder ? 'clickedInputLine' : 'inputLine'}
+              onChange={handleInput}
+              onFocus={() => handleFocusBorder('passwordBorder')}
+              onBlur={() => handleBlurBorder('passwordBorder')}
+            />
           </div>
-          <div className="contactInput">
+          <div className="phoneNumberInput">
             <div className="inputMessage">Phone Number *</div>
-            <input className="contact" />
+            <input
+              name="phoneNumber"
+              className={phoneNumberBorder ? 'clickedInputLine' : 'inputLine'}
+              onChange={handleInput}
+              onFocus={() => handleFocusBorder('phoneNumberBorder')}
+              onBlur={() => handleBlurBorder('phoneNumberBorder')}
+            />
           </div>
           <div className="addressInput">
             <div className="inputMessage">Address *</div>
-            <input className="address" />
+            <input
+              name="address"
+              className={addressBorder ? 'clickedInputLine' : 'inputLine'}
+              onChange={handleInput}
+              onFocus={() => handleFocusBorder('addressBorder')}
+              onBlur={() => handleBlurBorder('addressBorder')}
+            />
           </div>
         </form>
         <div className="agreeCheckbox">
           <div className="accountCheckboxAgreePolicy">
-            <input type="checkbox" className="checkbox" />
+            <input type="checkbox" className="checkbox" onClick={isClicked} />
             <span className="checkboxContent">
               I agree to the
               <span className="line">terms&conditions and privacy policy</span>*
@@ -55,7 +165,11 @@ function SignUp() {
         </div>
       </div>
       <div className="signUpBottom">
-        <button className="signUpButton" type="button">
+        <button
+          className={handleButtonAction()}
+          type="button"
+          disabled={!getIsActive()}
+        >
           CREATE USER ACCOUNT
         </button>
         <span className="alreadyConfirm">

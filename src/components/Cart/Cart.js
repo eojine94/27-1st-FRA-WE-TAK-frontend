b54978eca1597b54d1886b9102a0a8c03f78de06
leejihyun <ix219@naver.com>
2021-12-03 14:24:05 +0900

    Modify : navbar에 링크 추가 && 장바구니 창에 담을 상품을 컴포넌트로 구성(=CartItem)

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,77 +1,20 @@
 import React from 'react';
 import './Cart.scss';
+import CartItem from '../CartItem/CartItem.js';
 
 function Cart({ closeCartMenu }) {
   return (
     <div className="cart" onMouseLeave={closeCartMenu}>
       <h2 className="cartListTitle">Cart List</h2>
-      <ul className="cartDetail">
+      <ul className="cartListDetail">
         <li className="itemPhoto">photo</li>
         <li className="itemNum">num</li>
         <li className="itemPrice">price</li>
         <li className="itemDelete">delete</li>
       </ul>
-      <div class="itemList">
-        <ul className="itemDetail">
-          <li className="itemPhoto">
-            <img
-              className="eggItemImage"
-              src="../../image/logo.png"
-              alt="eggItem"
-            />
-          </li>
-          <li className="itemNum">1</li>
-          <li className="itemPrice">8,000</li>
-          <li className="itemDelete">x</li>
-        </ul>
-        <ul className="itemDetail">
-          <li className="itemPhoto">
-            <img
-              className="eggItemImage"
-              src="../../image/logo.png"
-              alt="eggItem"
-            />
-          </li>
-          <li className="itemNum">1</li>
-          <li className="itemPrice">5,000</li>
-          <li className="itemDelete">x</li>
-        </ul>
-        <ul className="itemDetail">
-          <li className="itemPhoto">
-            <img
-              className="eggItemImage"
-              src="../../image/logo.png"
-              alt="eggItem"
-            />
-          </li>
-          <li className="itemNum">2</li>
-          <li className="itemPrice">1000,000</li>
-          <li className="itemDelete">x</li>
-        </ul>
-        <ul className="itemDetail">
-          <li className="itemPhoto">
-            <img
-              className="eggItemImage"
-              src="../../image/logo.png"
-              alt="eggItem"
-            />
-          </li>
-          <li className="itemNum">1</li>
-          <li className="itemPrice">5,000</li>
-          <li className="itemDelete">x</li>
-        </ul>
-        <ul className="itemDetail">
-          <li className="itemPhoto">
-            <img
-              className="eggItemImage"
-              src="../../image/logo.png"
-              alt="eggItem"
-            />
-          </li>
-          <li className="itemNum">1</li>
-          <li className="itemPrice">5,000</li>
-          <li className="itemDelete">x</li>
-        </ul>
+      <div class="cartItems">
+        <CartItem />
+        <CartItem />
       </div>
     </div>
   );

7cc06596bcd081e647a5d31b75701564bc37c8e1
niboo <moonevening8@gmail.com>
2021-12-09 17:47:50 +0900

    REFECTOR: 서버통신 확인완료

--- src/Router.js
+++ src/Router.js
@@ -3,12 +3,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Main from './pages/Main/Main';
 import Login from './pages/Login/Login';
-import SignUp from './pages/SignUp/Signup';
+import SignUp from './pages/SignUp/SignUp';
 import List from './pages/List/List';
 import Detail from './pages/Detail/Detail';
 import FindId from './pages/FindId/FindId';
-import FindPassword from './pages/FindPassword/FindPassword';
-import ResetPassword from './pages/ResetPassword/ResetPassword';
 import PrintEmail from './pages/PrintEmail/PrintEmail';
 import MyPage from './pages/MyPage/MyPage';
 
@@ -23,8 +21,6 @@ function Router() {
         <Route path="/find-id" element={<FindId />} />
         <Route path="/list/:main_category_name" element={<List />} />
         <Route path="/detail/:product_id" element={<Detail />} />
-        <Route path="/find-password" element={<FindPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/print-email" element={<PrintEmail />} />
         <Route path="my-page" element={<MyPage />} />
       </Routes>
--- src/pages/Config/Config.js
+++ src/pages/Config/Config.js
@@ -4,8 +4,6 @@ const API = {
   SIGN_UP: `${BASE_URL}/users/signup`,
   LOG_IN: `${BASE_URL}/users/login`,
   FIND_ID: `${BASE_URL}/users/usersearch`,
-  FIND_PASSWORD: `${BASE_URL}/users/find-password`,
-  RESET_PASSWORD: `${BASE_URL}/users/reset-password`,
 };
 
 export default API;
